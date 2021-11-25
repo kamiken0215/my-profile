de9e6b99cdfb7e5007d81b3242e84e13ee2323b5
Kentaro Kamiyama <kamiken0215@gmail.com>
2021-11-25 18:57:01 +0900

    Grid化

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
-import { Box, Center, Text } from "@chakra-ui/react";
+import { Box, Center, Flex, Grid, GridItem, Img, Text } from "@chakra-ui/react";
 import { getProfile } from "../libs/profile";
 import { Profile, ProfileContents } from "../models/profile";
 
 export default function Home({ contents }: { contents: Profile[] }) {
   return (
-    <Center>
-      <Box w={["90vw", "50vw"]}>
+    <Grid templateColumns={["1fr", "repeat(4, 1fr)"]} gap={2}>
+      <GridItem colStart={[1, 2]} colEnd={[2, 3]}>
+        <Flex alignContent="center" justifyContent="center">
+          <Img
+            w={["50%", "80%"]}
+            h={["100%", "120%"]}
+            src="/images/prof.jpg"
+          ></Img>
+        </Flex>
+      </GridItem>
+      <GridItem colStart={[1, 3]} colEnd={[2, 4]}>
         {contents.map((value, index) => (
           <Text key={index}>{value.title}</Text>
         ))}
-      </Box>
-    </Center>
+      </GridItem>
+    </Grid>
   );
 }
 
